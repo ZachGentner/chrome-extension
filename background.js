3aa9b546cf71426e41d10c3030104b4e88aa1335
Zach Gentner <zach.gentner@gmail.com>
2023-11-10 20:38:01 -0500

    context menu template

---
+++ background.js
@@ -0,0 +1,23 @@
+// Check if the chrome namespace is available
+if (typeof chrome !== 'undefined') {
+    // Add an event listener for when the extension is installed or updated
+    chrome.runtime.onInstalled.addListener(() => {
+        // Create a context menu item
+        chrome.contextMenus.create({
+            id: 'Rootwise',
+            title: 'Rootwise',
+            contexts: ['page', 'selection', 'link', 'image', 'video', 'audio'],
+        });
+    });
+
+    // Add a listener for when the context menu item is clicked
+    chrome.contextMenus.onClicked.addListener((info, tab) => {
+        if (info.menuItemId === 'Rootwise') {
+        // Handle the context menu click
+            console.log('Context menu clicked!');
+            console.log('Selected text:', info.selectionText);
+        }
+    });
+} else {
+    console.error('Chrome extension API not available.');
+}
