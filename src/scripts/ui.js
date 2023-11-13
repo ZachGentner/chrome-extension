f001a5274425702c836de5a635ad735fea0fcb29
Zach Gentner <zach.gentner@gmail.com>
2023-11-13 13:34:57 -0500

    file structure

---
+++ src/scripts/ui.js
@@ -0,0 +1,118 @@
+import * as data from './data.js';
+
+export function updateName(fullName, element) {
+  return (element.innerText = fullName);
+}
+
+export function updateId(id, element) {
+  return (element.innerText = `ID: ${id}`);
+}
+
+export function updateLifespan(birth, death, element) {
+  if (birth === undefined && death === undefined) {
+    return (element.innerText = 'No dates');
+  }
+
+  const birthYear = birth != undefined ? birth : '';
+  const deathYear = death != undefined ? death : '';
+
+  return (element.innerText = `${birthYear} - ${deathYear}`);
+}
+
+export function updateLinks(links, element) {
+  if (links.ancestry.url === undefined) {
+    element.querySelector('#ancestry').querySelector('a').href =
+      links.ancestry.default;
+    element.querySelector('#ancestry').disabled = true;
+  } else {
+    element.querySelector('#ancestry').querySelector('a').href =
+      links.ancestry.url;
+    element.querySelector('#ancestry').disabled = false;
+  }
+
+  if (links.familysearch.url === undefined) {
+    element.querySelector('#familysearch').querySelector('a').href =
+      links.familysearch.default;
+    element.querySelector('#familysearch').disabled = true;
+  } else {
+    element.querySelector('#familysearch').querySelector('a').href =
+      links.familysearch.url;
+    element.querySelector('#familysearch').disabled = false;
+  }
+
+  if (links.findagrave.url === undefined) {
+    element.querySelector('#findagrave').querySelector('a').href =
+      links.findagrave.default;
+    element.querySelector('#findagrave').disabled = true;
+  } else {
+    element.querySelector('#findagrave').querySelector('a').href =
+      links.findagrave.url;
+    element.querySelector('#findagrave').disabled = false;
+  }
+}
+
+// Opens a new tab with the url passed as an argument.
+export function openNewTab(url) {
+  return window.open(url, '_blank').focus();
+}
+
+// Updates the info bar with information about the active person.
+export function renderActive(element) {
+  updateName(
+    data.getFullName(data.active),
+    element.querySelector('#info').querySelector('#name'),
+  );
+  updateId(
+    data.findId(data.active),
+    element.querySelector('#info').querySelector('#id'),
+  );
+  updateLifespan(
+    data.getBirthYear(data.active),
+    data.getDeathYear(data.active),
+    element.querySelector('#info').querySelector('#lifespan'),
+  );
+  updateLinks(
+    data.getAllLinks(data.active),
+    element.querySelector('#quicklinks'),
+  );
+}
+
+// Creates filtered search results based on which people meet the input criteria.
+export function filterResults(people, search, element) {
+  if (people.length >= 1) {
+    people.forEach((id) => {
+      const person = document.createElement('li'); // Create a new list item for the person.
+      person.setAttribute('id', data.findId(id));
+
+      // Add an onclick event listener for the element.
+      person.addEventListener('click', () => {
+        data.setActive(person.getAttribute('id')); // Set the element id to the person id.
+        renderActive(element.parentElement); // Render active passing in body element.
+        person.parentElement.style.display = 'none'; // Hide the results menu.
+        person.parentElement.parentElement.querySelector('#input').value = '';
+        person.parentElement.innerHTML = ''; // Clear the results menu.
+      });
+
+      const birthSurname =
+        id.maiden.toLowerCase().startsWith(search.toLowerCase()) &&
+        id.maiden.length >= 1
+          ? id.maiden
+          : id.surname;
+      person.innerText = `${birthSurname}, ${id.first} ${id.middle}`;
+      element.appendChild(person);
+    });
+  }
+}
+
+// function updatePhoto(photoUrl, photoFrame) {
+//     if (photoUrl !== '' && photoUrl !== undefined) {
+//         const img = document.createElement('img');
+//         img.src = photoUrl;
+//         photoFrame.querySelector('i').remove();
+//         photoFrame.appendChild(img);
+//     } else if (!document.querySelector('#profile').querySelector('i')) {
+//         const icon = document.createElement('i');
+//         icon.className = 'fa-regular fa-user fa-lg';
+//         photoFrame.appendChild(icon);
+//     }
+// }
